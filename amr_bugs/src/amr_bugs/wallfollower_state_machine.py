956996dfeaba8178ceb578aa88dc314d01b400f3
Debaraj Barua <debaraj.barua@smail.inf.h-brs.de>
2017-05-27 13:31:15 +0200

    wallfollower statemachine bug fix for left wall follow

--- amr_bugs/src/amr_bugs/wallfollower_state_machine.py
+++ amr_bugs/src/amr_bugs/wallfollower_state_machine.py
@@ -1,35 +1,14 @@
 #!/usr/bin/env python
-
-"""
-This module provides a single construct() function which produces a Smach state
-machine that implements wallfollowing behavior.
-
-The state machine contains three states:
-    * findWall:     initial state - drives until a wall is detected
-    * alignWall     aligning state - used to align at convex corners or walls in front of robot 
-    * followWall    following state - used to follow a straight wall, robust against sensor noise, curls around concave corners
-
-The constructed state machine has three attached methods:
-    * set_ranges(ranges): this function should be called to update the range
-                          readings
-    * get_twist(): returns a twist message that could be directly passed to the
-                   velocity publisher
-    * set_config(config): updates the machine userdata with the new config
-
-The constructed state machine is preemptable, i.e. each state checks whether
-a preemption is requested and returns 'preempted' if that is the case.
-"""
-
 """
 #==============================================================================
 #                       State Descriptions
 #                       ------------------
 #   1) SEARCH            : Used to search for any wall, used during initialization
 #   2) ALIGN             : Used to alignt to a wall, accroding to mode selected
-#   3) FOLLOW_WALL       : Used to follow a wall once aligned 
+#   3) FOLLOW_WALL       : Used to follow a wall once aligned & also navigate 
+                           convex turns
 #   4) TRANSITION_SEARCH : If mode is changed during wall following, this is 
 #                          used to search for wall in the required direction
-#   5) CONVEX            : Used ro navigate door ways, i.e, convex turns
 #   5) CONCAVE           : Used to navigate corners, i.e., concave turns 
 #==============================================================================
 """
@@ -41,35 +20,12 @@ roslib.load_manifest(PACKAGE)
 import smach
 import rospy
 from preemptable_state import PreemptableState
-from math import copysign
+#from math import copysign
 from types import MethodType
 from geometry_msgs.msg import Twist
 
 
 __all__ = ['construct']
-
-#=============================== YOUR CODE HERE ===============================
-# Instructions: write a function for each state of wallfollower state machine.
-#               The function should have exactly one argument (userdata
-#               dictionary), which you should use to access the input ranges
-#               and to provide the output velocity.
-#               The function should have at least one 'return' statement, which
-#               returns one of the possible outcomes of the state.
-#               The function should not block (i.e. have infinite loops), but
-#               rather it should implement just one iteration (check
-#               conditions, compute velocity), because it will be called
-#               regularly from the state machine.
-#
-# Hint: below is an example of a state that moves the robot forward until the
-#       front sonar readings are less than the desired clearance. It assumes
-#       that the corresponding variables ('front_min' and 'clearance') are
-#       available in the userdata dictionary.
-#
-#           def search(ud):
-#               if ud.front_min < ud.clearance:
-#                   return 'found_obstacle'
-#               ud.velocity = (1, 0, 0)
-#==============================================================================
 
 #==============================================================================
 #    SEARCH            : Used to search for any wall, used during initialization
@@ -88,35 +44,6 @@ def set_ranges(self, ranges):
     For left hand side wallfollowing, the sensor values are mirrored (sides are swapped).
     """
 
-
-    #============================= YOUR CODE HERE =============================
-    # Instructions: store the ranges from a ROS message into the userdata
-    #               dictionary of the state machine.
-    #               'ranges' is a list or Range messages (that should be
-    #               familiar to you by now). It implies that to access the
-    #               actual range reading of, say, sonar number 3, you need to
-    #               write:
-    #
-    #                   ranges[3].range
-    #
-    #               For example, to create an item called 'front_min', which
-    #               contains the minimum between the ranges reported by the two
-    #               front sonars, you would write the following:
-    #
-    #                   self.userdata.front_min = min(ranges[3].range, ranges[4].range)
-    #
-    # Hint: you can just store the whole array of the range readings, but to
-    #       simplify the code in your state functions, you may compute
-    #       additional values, e.g. the difference between the reading of the
-    #       side sonars, or the minimum of all sonar readings, etc.
-    #
-    # Hint: you can access all the variables stored in userdata. This includes
-    #       the current settings of the wallfollower (that is clearance and the
-    #       mode of wallfollowing). Think about how you could make your state
-    #       functions independent of wallfollowing mode by smart preprocessing
-    #       of the sonar readings.
-    #==========================================================================
-    
     self.userdata.front_1 = ranges[3].range
     self.userdata.front_2 = ranges[4].range
     self.userdata.front_min = min(ranges[3].range, ranges[4].range)
@@ -143,17 +70,6 @@ def get_twist(self):
     twist.angular.x = 0
     twist.angular.y = 0
     twist.angular.z = self.userdata.velocity[2]
-
-    #============================= YOUR CODE HERE =============================
-    # Instructions: although this function is implemented, you may need to
-    #               slightly tweak it if you decided to handle wallfolllowing
-    #               mode in "the smart way".
-    # Hint: state machine userdata is accessible in this function as well, for
-    #       example you can read the current wallfollowing mode with
-    #
-    #           self.userdata.mode
-    #
-    #==========================================================================
 
     return twist
 
@@ -269,13 +185,13 @@ def follow_wall(userdata):
             f_corner>userdata.clearance*5):    #if it get lost then go find a wall
             return 'no_wall'
         if(abs(f_corner)>userdata.clearance*5 or max(side_1,side_2)>userdata.clearance+tolerance):  #if there is opening then go to door
-            #return 'convex'          
-             rospy.loginfo("Convex Turning")
-             userdata.velocity=(userdata.max_forward_velocity,
-                                     userdata.clearance-min(side_1,side_2),
-                                     (side_2-side_1)*1.7 )
-
-        
+          
+            rospy.loginfo("Convex Turning")
+            rospy.logwarn(userdata.direction*(userdata.clearance-min(side_1,side_2)))
+            userdata.velocity=(userdata.max_forward_velocity,
+                                     userdata.direction*(userdata.clearance-min(side_1,side_2)),
+                                     userdata.direction*(side_2-side_1)*1.7 )
+             
          
         #==============================================================================
         #         Aligning Robot for uneven walls        
@@ -284,13 +200,16 @@ def follow_wall(userdata):
         #align lateral offsets to wall
         if (((max(side_2,side_1)>userdata.clearance+tolerance) and back_side>userdata.clearance+tolerance)
             or (min(side_2,side_1)<userdata.clearance+tolerance and back_side<userdata.clearance+tolerance)):  
+               
                rospy.loginfo("Correcting Offsets")  
-               userdata.velocity=(0.01, userdata.direction*userdata.clearance-min(side_1,side_2),userdata.direction*(side_2-side_1))          
-               #userdata.velocity=(0.01, userdata.clearance-min(side_1,side_2) ,0)  
+               rospy.logwarn(userdata.direction*(userdata.clearance-min(side_1,side_2)))
+               
+               userdata.velocity=(0.01, userdata.direction*(userdata.clearance-min(side_1,side_2)),userdata.direction*(side_2-side_1)) 
 
         #rotate while moving to maitain orientation
         if(abs(side_1-side_2)>0.02 and side_2<userdata.clearance+tolerance 
             and side_1<userdata.clearance+tolerance):   
+            
             rospy.loginfo("Following with angular adjustments")            
             userdata.velocity=(0.3,0,
                             ((side_2-side_1)/abs(side_2-side_1))*angular_velo)
@@ -298,6 +217,7 @@ def follow_wall(userdata):
         # go straight       
         elif side_1==userdata.clearance+tolerance :                                                      
             rospy.loginfo("Following Straight")
+            
             userdata.velocity=(userdata.max_forward_velocity,0,0)
         
         
@@ -331,62 +251,6 @@ def concave(userdata):
                 userdata.velocity=(0,0,0)
                 return 'navigated'
         
-#==============================================================================
-# Convex State is called to manoeuvre corners with gaps in walls (like doors)
-#==============================================================================
-def convex(userdata):        
-    
-        #==============================================================================
-        #         Find direction to be alligned to, left or right
-        #==============================================================================        
-        if(userdata.direction==1):  #follow on right
-        #check fot the clearance with repect to  the corner sensors 
-            if(userdata.front_right>userdata.clearance*3 or max(userdata.right_2,userdata.right_1)>userdata.clearance):
-                    
-#==============================================================================
-#                 userdata.velocity=(userdata.max_forward_velocity,
-#                                     userdata.max_forward_velocity/7,
-#                                     -userdata.default_rotational_speed)
-#==============================================================================
-                 rospy.loginfo("Navigating Convex Right")
-                 
-                 #if(min(userdata.left_1,userdata.left_2))
-                 
-                 userdata.velocity=(userdata.max_forward_velocity,
-                                    #(userdata.clearance-min(userdata.left_1,userdata.left_2))*0.05,
-                                    0,
-                                    (userdata.right_2-userdata.right_1)*1.7 )
-                
-                
-            else:
-                userdata.velocity=(0,0,0)
-                return 'navigated'
-        
-        else:   #follow on left
-            if(userdata.front_left>userdata.clearance*2 or max(userdata.left_2,userdata.left_1)>userdata.clearance):
-                
-#==============================================================================
-#                 userdata.velocity=(userdata.max_forward_velocity,
-#                                     -userdata.max_forward_velocity/7,
-#                                     userdata.default_rotational_speed) 
-#                 
-#==============================================================================
-                
-                 userdata.velocity=(userdata.max_forward_velocity,
-                                    #(userdata.clearance-min(userdata.left_1,userdata.left_2))*0.05,
-                                    0,
-                                    (userdata.left_2-userdata.left_1)*1.7 ) 
-                
-            else:
-                userdata.velocity=(0,0,0)
-                return 'navigated'
-"""
-def stop(userdata):
-   
-    rospy.loginfo("Stopping")
-    userdata.velocity = (0, 0, 0)
-    return 'stop'
-"""
 def construct():
     sm = smach.StateMachine(outcomes=['preempted'])
     # Attach helper functions
@@ -404,33 +268,6 @@ def construct():
 
     with sm:
         #pass
-        #=========================== YOUR CODE HERE ===========================
-        # Instructions: construct the state machine by adding the states that
-        #               you have implemented.
-        #               Below is an example how to add a state:
-        #
-        #                   smach.StateMachine.add('SEARCH',
-        #                                          PreemptableState(search,
-        #                                                           input_keys=['front_min', 'clearance'],
-        #                                                           output_keys=['velocity'],
-        #                                                           outcomes=['found_obstacle']),
-        #                                          transitions={'found_obstacle': 'ANOTHER_STATE'})
-        #
-        #               First argument is the state label, an arbitrary string
-        #               (by convention should be uppercase). Second argument is
-        #               an object that implements the state. In our case an
-        #               instance of the helper class PreemptableState is
-        #               created, and the state function in passed. Moreover,
-        #               we have to specify which keys in the userdata the
-        #               function will need to access for reading (input_keys)
-        #               and for writing (output_keys), and the list of possible
-        #               outcomes of the state. Finally, the transitions are
-        #               specified. Normally you would have one transition per
-        #               state outcome.
-        #
-        # Note: The first state that you add will become the initial state of
-        #       the state machine.
-        #======================================================================
         
         #==============================================================================
         # SEARCH            : Used to search for any wall, used during initialization        
@@ -461,10 +298,9 @@ def construct():
                                                                                       'direction','default_rotational_speed',
                                                                                       'max_forward_velocity','back_right','back_left'],
                                                                    output_keys=['velocity'],
-                                                                   outcomes=['concave','no_wall','convex','align']),
+                                                                   outcomes=['concave','no_wall','align']),
                                                                    transitions={'concave': 'CONCAVE',
                                                                                  'no_wall':'TRANSITION_SEARCH',
-                                                                                 'convex':'CONVEX',
                                                                                  'align' : 'ALIGN'})
                                                                                  
         #==============================================================================
@@ -492,25 +328,5 @@ def construct():
                                                                     outcomes=['navigated']),
                                                                     transitions={'navigated': 'FOLLOW_WALL' })
 
-        #==============================================================================
-        # Convex State is called to manoeuvre corners with gaps in walls (like doors)
-        #==============================================================================                                                                    
-        smach.StateMachine.add('CONVEX', PreemptableState(convex,input_keys=['front_min', 'clearance','max_forward_velocity',
-                                                                               'front_1','front_2',
-                                                                                'left_1','left_2',
-                                                                                'right_1','right_2',
-                                                                                'front_right','front_left',
-                                                                                'direction','default_rotational_speed'],
-                                                                    output_keys=['velocity'],
-                                                                    outcomes=['navigated']),
-                                                                    transitions={'navigated': 'FOLLOW_WALL' })                                                                    
-        
-        """
-        #STOP state was used as a redundant temporary state for undefined states
-        smach.StateMachine.add('STOP',PreemptableState(stop,input_keys=['front_min','clearance'],
-                                                                   output_keys=['velocity'],
-                                                                   outcomes=['stop']),
-                                                                    transitions={'stop': 'STOP'})
-        """           
-    
+      
     return sm
